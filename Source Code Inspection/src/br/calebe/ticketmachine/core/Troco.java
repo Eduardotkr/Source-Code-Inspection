4a0d99bf68b2b656e4d4063eda3af907c3cce1d4
31159257 <31159257@099246-P31S303.macklabs.br>
2014-10-08 19:34:38 -0300

    modificado classe troco

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -1,5 +1,6 @@
 package br.calebe.ticketmachine.core;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
 /**
@@ -8,79 +9,44 @@ import java.util.Iterator;
  */
 class Troco {
 
-    protected PapelMoeda[] papeisMoeda;
+    protected ArrayList<PapelMoeda> papeisMoeda;
 
     public Troco(int valor) {
-        papeisMoeda = new PapelMoeda[6];
+        papeisMoeda = new ArrayList<>();
         int count = 0;
         while (valor % 100 != 0) {
             count++;
         }
-        papeisMoeda[5] = new PapelMoeda(100, count);
+        papeisMoeda.add(5, new PapelMoeda(100, count));
         count = 0;
         while (valor % 50 != 0) {
             count++;
         }
-        papeisMoeda[4] = new PapelMoeda(50, count);
+        papeisMoeda.add(4, new PapelMoeda(50, count));
         count = 0;
         while (valor % 20 != 0) {
             count++;
         }
-        papeisMoeda[3] = new PapelMoeda(20, count);
+        papeisMoeda.add(3, new PapelMoeda(20, count));
         count = 0;
         while (valor % 10 != 0) {
             count++;
         }
-        papeisMoeda[2] = new PapelMoeda(10, count);
+        papeisMoeda.add(2, new PapelMoeda(10, count));
         count = 0;
         while (valor % 5 != 0) {
             count++;
         }
-        papeisMoeda[1] = new PapelMoeda(5, count);
+        papeisMoeda.add(1, new PapelMoeda(5, count));
         count = 0;
         while (valor % 2 != 0) {
             count++;
         }
-        papeisMoeda[1] = new PapelMoeda(2, count);
+        papeisMoeda.add(1, new PapelMoeda(2, count));
     }
 
     public Iterator<PapelMoeda> getIterator() {
         return new TrocoIterator(this);
     }
 
-    class TrocoIterator implements Iterator<PapelMoeda> {
-
-        protected Troco troco;
-
-        public TrocoIterator(Troco troco) {
-            this.troco = troco;
-        }
-
-        @Override
-        public boolean hasNext() {
-            for (int i = 6; i >= 0; i++) {
-                if (troco.papeisMoeda[i] != null) {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        @Override
-        public PapelMoeda next() {
-            PapelMoeda ret = null;
-            for (int i = 6; i >= 0 && ret != null; i++) {
-                if (troco.papeisMoeda[i] != null) {
-                    ret = troco.papeisMoeda[i];
-                    troco.papeisMoeda[i] = null;
-                }
-            }
-            return ret;
-        }
-
-        @Override
-        public void remove() {
-            next();
-        }
-    }
 }
