a5e2eed3a5d605dd3e5d47857312e384d7f750b7
31159257 <31159257@099083-P31S303.macklabs.br>
2014-10-08 22:11:41 -0300

    Adicionado classe teste

--- Source Code Inspection/test/br/calebe/ticketmachine/core/TrocoTest.java
+++ Source Code Inspection/test/br/calebe/ticketmachine/core/TrocoTest.java
@@ -42,12 +42,24 @@ public class TrocoTest {
      */
     @Test
     public void testGetIterator() {
-        System.out.println("getIterator");
-        Troco instance = null;
-        Iterator expResult = null;
-        Iterator result = instance.getIterator();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        PapelMoeda[] papelMoedaList = new PapelMoeda[6];
+        papelMoedaList[5] = new PapelMoeda(100, 1);
+        papelMoedaList[4] = new PapelMoeda(50, 1);
+        papelMoedaList[3] = new PapelMoeda(20, 1);
+        papelMoedaList[2] = new PapelMoeda(10, 1);
+        papelMoedaList[1] = new PapelMoeda(5, 1);
+        papelMoedaList[0] = new PapelMoeda(2, 1);
+        int i = 5;
+        Troco troco = new Troco(187);
+        Iterator result = troco.getIterator();
+        while(result.hasNext()){
+            Object next = result.next();
+            assertEquals(papelMoedaList[i].getQuantidade(), ((PapelMoeda)next).getQuantidade());
+            assertEquals(papelMoedaList[i].getValor(), ((PapelMoeda)next).getValor());
+            System.out.println("QUANTIDADE - : " + ((PapelMoeda)next).getQuantidade());
+            System.out.println("VALOR - : " + ((PapelMoeda)next).getValor());
+            i--;
+        }
     }
 }
